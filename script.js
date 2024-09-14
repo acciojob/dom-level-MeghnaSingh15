c763b530fb83fc8374b45a937c09adfab32818a1
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-14 12:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let n = document.getElementById("level");
+let level = 0;
+while(n.parentNode){
+	level++;
+	n = n.parentNode;	
+}
+alert("The level of the element is" + " " + level);
